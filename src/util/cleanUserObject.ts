82606545c3da3d374ccb83215504b9b6848fe8c9
BlazingAsher <opensource@davidhui.ca>
2025-07-06 19:14:53 -0400

    log way less about user in request and response

--- src/util/cleanUserObject.ts
+++ src/util/cleanUserObject.ts
@@ -1,9 +1,15 @@
 import {IApplication, IUser} from "../models/user/fields";
 
-export const cleanUserObject = (user?: IUser):IUser|undefined => {
+export const cleanUserObject = (user?: IUser):Record<string, any>|undefined => {
     if(user?.["hackerApplication"] !== undefined) {
         user["hackerApplication"] = {} as IApplication;
     }
 
-    return user;
+    return {
+        groups: user?.groups,
+        roles: user?.roles,
+        _id: user?._id,
+        email: user?.email,
+        created: user?.created,
+    }
 }
